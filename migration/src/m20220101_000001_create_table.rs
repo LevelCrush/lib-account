c2e45ffa89bf8e2b30946b056647f3829988e11f
Gabriele Nunez <gabrielenunez@protonmail.com>
2024-03-02 13:42:57 -0500

    Finished initial port of schema into sea-orm migration

--- migration/src/m20220101_000001_create_table.rs
+++ migration/src/m20220101_000001_create_table.rs
@@ -20,11 +20,15 @@ impl MigrationTrait for Migration {
                     )
                     .col(
                         ColumnDef::new(Accounts::Token)
-                            .text()
+                            .char_len(32)
                             .not_null()
                             .unique_key(),
                     )
-                    .col(ColumnDef::new(Accounts::TokenSecret).text().not_null())
+                    .col(
+                        ColumnDef::new(Accounts::TokenSecret)
+                            .char_len(32)
+                            .not_null(),
+                    )
                     .col(
                         ColumnDef::new(Accounts::Admin)
                             .boolean()
@@ -51,6 +55,137 @@ impl MigrationTrait for Migration {
                             .name("accounts-admin")
                             .table(Accounts::Table)
                             .col(Accounts::Admin),
+                    )
+                    .to_owned(),
+            )
+            .await;
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(AccountPlatforms::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(AccountPlatforms::Id)
+                            .big_integer()
+                            .not_null()
+                            .primary_key()
+                            .auto_increment(),
+                    )
+                    .col(ColumnDef::new(AccountPlatforms::Platform).text().not_null())
+                    .col(
+                        ColumnDef::new(AccountPlatforms::Token)
+                            .char_len(32)
+                            .not_null()
+                            .unique_key(),
+                    )
+                    .col(
+                        ColumnDef::new(AccountPlatforms::PlatformUser)
+                            .text()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(AccountPlatforms::CreatedAt)
+                            .big_integer()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(AccountPlatforms::UpdatedAt)
+                            .big_integer()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(AccountPlatforms::DeletedAt)
+                            .big_integer()
+                            .not_null(),
+                    )
+                    .index(
+                        Index::create()
+                            .if_not_exists()
+                            .table(AccountPlatforms::Table)
+                            .name("accountplatforms-account-platform")
+                            .col(AccountPlatforms::Account)
+                            .col(AccountPlatforms::Platform)
+                            .unique(),
+                    )
+                    .index(
+                        Index::create()
+                            .if_not_exists()
+                            .table(AccountPlatforms::Table)
+                            .name("accountplatforms-platform")
+                            .col(AccountPlatforms::Platform),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .from(AccountPlatforms::Table, AccountPlatforms::Account)
+                            .to(Accounts::Table, Accounts::Id),
+                    ),
+            )
+            .await;
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(AccountPlatformData::Table)
+                    .col(
+                        ColumnDef::new(AccountPlatformData::Id)
+                            .big_integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(
+                        ColumnDef::new(AccountPlatformData::Account)
+                            .big_integer()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(AccountPlatformData::Platform)
+                            .big_integer()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(AccountPlatformData::Key)
+                            .string_len(128)
+                            .not_null(),
+                    )
+                    .col(ColumnDef::new(AccountPlatformData::Value).text().not_null())
+                    .index(
+                        Index::create()
+                            .name("apdata-account-platform")
+                            .table(AccountPlatformData::Table)
+                            .col(AccountPlatformData::Account)
+                            .col(AccountPlatformData::Platform),
+                    )
+                    .index(
+                        Index::create()
+                            .name("apdata-account-platform-data-key")
+                            .table(AccountPlatformData::Table)
+                            .col(AccountPlatformData::Account)
+                            .col(AccountPlatformData::Platform)
+                            .col(AccountPlatformData::Key),
+                    )
+                    .index(
+                        Index::create()
+                            .name("apdata-key")
+                            .table(AccountPlatformData::Table)
+                            .col(AccountPlatformData::Key),
+                    )
+                    .index(
+                        Index::create()
+                            .name("apdata-platform")
+                            .table(AccountPlatformData::Table)
+                            .col(AccountPlatformData::Platform),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .from(AccountPlatformData::Table, AccountPlatformData::Account)
+                            .to(Accounts::Table, Accounts::Id),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .from(AccountPlatformData::Platform, AccountPlatformData::Platform)
+                            .to(AccountPlatforms::Table, AccountPlatforms::Id),
                     )
                     .to_owned(),
             )
