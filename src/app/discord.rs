ac4bea2b333b88f668aa3d0657627f68a519bc37
Gabriele Nunez <gabrielenunez@protonmail.com>
2024-06-02 07:03:37 -0400

    Format

--- src/app/discord.rs
+++ src/app/discord.rs
@@ -53,10 +53,7 @@ pub async fn validate_oauth(
 }
 
 /// queries a discord user directly by their discord id
-pub async fn member_api(
-    discord_id: &str,
-    state: &ApplicationState<AccountExtension>,
-) -> Option<DiscordUserResponse> {
+pub async fn member_api(discord_id: &str, state: &ApplicationState<AccountExtension>) -> Option<DiscordUserResponse> {
     let bot_token = state.extension.discord_bot_token.clone();
     let bot_auth = format!("Bot {}", bot_token);
     let discord_user_id = discord_id;
@@ -85,10 +82,7 @@ pub async fn member_api(
 }
 
 /// fetches a discord member by their id and syncs the result
-pub async fn member(
-    discord_id: &str,
-    state: &ApplicationState<AccountExtension>,
-) -> Option<MemberSyncResult> {
+pub async fn member(discord_id: &str, state: &ApplicationState<AccountExtension>) -> Option<MemberSyncResult> {
     let discord_response = member_api(discord_id, state).await;
     if let Some(user_response) = discord_response {
         sync::discord::member(user_response, &state).await
@@ -124,12 +118,22 @@ pub async fn member_oauth_api(
     }
 }
 
+/// Query the discord api directly and get the currently logged in users guild list
+pub async fn member_oauth_guilds_api(access_token: &str, state: &ApplicationState<AccountExtension>) -> Option<()> {
+    let request = state
+        .extension
+        .http_client
+        .get("https://discord.com/api/v10/users/@me/guilds")
+        .bearer_auth(access_token)
+        .send()
+        .await;
+
+    None
+}
+
 /// Query a member via oauth authentication
 /// Update them in our database
-pub async fn member_oauth(
-    access_token: &str,
-    state: &ApplicationState<AccountExtension>,
-) -> Option<MemberSyncResult> {
+pub async fn member_oauth(access_token: &str, state: &ApplicationState<AccountExtension>) -> Option<MemberSyncResult> {
     let oauth_response = member_oauth_api(access_token, state).await;
     if let Some(user_response) = oauth_response {
         sync::discord::member(user_response, &state).await
